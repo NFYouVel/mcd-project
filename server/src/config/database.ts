986fabd8eeb7015bc846f23c6bc9e13f40b2fd3a
Marvel Nathanael <marvelnathanael072006@gmail.com>
2026-04-29 18:53:37 +0700

    commit database

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,14 +1,5 @@
 import { Sequelize } from 'sequelize-typescript';
-import { Users } from '../models/Users.js';
-import { Orders } from '../models/orders.js';
-import { Payment } from '../models/payment.js';
-import { Type } from '../models/Type.js';
-import { OrderItems } from '../models/OrderItems.js';
-import { MenuSection } from '../models/menuSection.js';
-import { Menu } from '../models/menu.js';
-import { FilterMenu } from '../models/filterMenu.js';
-import { IngredientItems } from '../models/ingredientItems.js';
-import { Ingredients } from '../models/ingredients.js';
+import path from 'path';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -19,5 +10,5 @@ export const sequelize = new Sequelize({
     password: process.env.DB_PASSWORD as string,
     host: process.env.DB_HOST as string,
     dialect: 'postgres',
-    models: [Users, Orders, Payment, Type, MenuSection, FilterMenu, Menu, OrderItems, IngredientItems, Ingredients]
+    models: [path.join(__dirname, "../src/models")],
 });
